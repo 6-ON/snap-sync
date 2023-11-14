b52b5f28b4c0a43f64469eb59a6c9e04c68e9a22
6-ON <aminhatim6@gmail.com>
2023-11-14 16:08:48 +0100

    Add post controller and export it in index

---
+++ src/controllers/posts.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { PostDTO } from "../dto";
+import { PostService } from "../services";
+
+type PostRequest = Request<unknown, unknown, PostDTO>;
+
+export class PostController {
+	static async index(req: Request, res: Response) {
+		return res.status(200).json(await PostService.findAll());
+	}
+	// static async create(req: PostRequest, res: Response) {
+	// 	return res.status(201).json(req.body);
+	// }
+	// static async delete(req: Request, res: Response) {
+	// 	throw new Error("Function not implemented.");
+	// }
+}
