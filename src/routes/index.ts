6b56ba2c75987052c6d664d244805505a253ff04
6-ON <aminhatim6@gmail.com>
2023-11-14 16:09:16 +0100

    Add json middleware to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import { Router, json } from "express";
 import postsRouter from "./posts";
 
 const router = Router();
-
+router.use(json());
 router.use("/posts", postsRouter);
 
 export default router;
