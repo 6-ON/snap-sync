01fd26e682768ef2a4f7194366c2dd48bf1248b7
6-ON <aminhatim6@gmail.com>
2023-11-16 14:35:49 +0100

    Add default value for likes in Post schema.

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -4,7 +4,7 @@ export const postSchema = new Schema<PostDTO>({
 	title: { type: String, required: true },
 	content: { type: String, required: true },
 	image: { type: String, required: true },
-	likes: { type: Number, required: true },
+	likes: { type: Number, required: true, default: 0 },
 	tags: { type: [String], required: true },
 });
 
