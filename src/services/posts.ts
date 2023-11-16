29656190d878a29d66d7c4514a26774528d3fcfd
6-ON <aminhatim6@gmail.com>
2023-11-16 14:20:09 +0100

    Add error handling for post creation

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,16 +1,16 @@
-// import { PostDTO } from "src/dto";
-// import { Schema, model} from 'mongoose';
-
 import { PostDTO } from "src/dto";
 import { Post } from "../models";
-
+import { Error } from "mongoose";
+import { BadRequest } from "http-errors";
 export class PostService {
 	static async findAll() {}
 	static async create(post: PostDTO) {
 		try {
 			return await Post.create(post);
 		} catch (err) {
-			return err;
+			if (err instanceof Error.ValidationError)
+				throw new BadRequest(JSON.stringify(err));
+			throw err;
 		}
 	}
 	static async delete(post: PostDTO) {
