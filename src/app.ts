12a2a0a35f35e188648320bf03249dcba426af90
6-ON <aminhatim6@gmail.com>
2023-11-16 14:34:54 +0100

    Add config and error handling middleware to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
+import "./config";
 import router from "./routes";
+import { handleErrors } from "./middlewares";
 const app: Application = express();
 app.use(router);
-
+app.use(handleErrors);
 export default app;
