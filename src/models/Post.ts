be448f8cedcfdaae8edd5702d0560eb05954f700
6-ON <aminhatim6@gmail.com>
2023-11-16 16:32:03 +0100

    Add timestamps to Post schema

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,11 +1,14 @@
 import { Schema, model } from "mongoose";
 import { PostDTO } from "src/dto";
-export const postSchema = new Schema<PostDTO>({
-	title: { type: String, required: true },
-	content: { type: String, required: true },
-	image: { type: String, required: true },
-	likes: { type: Number, required: true, default: 0 },
-	tags: { type: [String], required: true },
-});
+export const postSchema = new Schema<PostDTO>(
+	{
+		title: { type: String, required: true },
+		content: { type: String, required: true },
+		image: { type: String, required: true },
+		likes: { type: Number, required: true, default: 0 },
+		tags: { type: [String], required: true },
+	},
+	{ timestamps: true },
+);
 
 export const Post = model<PostDTO>("Post", postSchema);
