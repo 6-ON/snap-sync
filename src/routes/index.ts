3dd18b5a8b61035d8134428a1f1bbde0239f74df
6-ON <aminhatim6@gmail.com>
2023-11-19 21:27:09 +0100

    Add cors package and configure CORS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router, json } from "express";
+import cors from "cors";
 import postsRouter from "./posts";
 
 const router = Router();
 router.use(json());
+router.use(cors({ origin: "http://localhost:3000" }));
 router.use("/posts", postsRouter);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-const port = 3000;
+const port = 5000;
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
