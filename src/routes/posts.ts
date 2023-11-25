17e6b4b2ac8420ec24bba9f790268f0f514be867
6-ON <aminhatim6@gmail.com>
2023-11-25 18:11:26 +0100

    Refactor posts routes and add auth and require owner  middleware

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import { PostController } from "../controllers";
+import { PostController } from "@/controllers";
+import { requirePostOwner, verifyJwt } from "@/middlewares";
+import { postParam } from "./params/post";
+
 const postsRouter = Router();
+
+postsRouter.param("post", postParam);
 postsRouter.get("/", PostController.index);
-postsRouter.post("/", PostController.create);
 postsRouter.get("/:post", PostController.show);
-postsRouter.put("/:post", PostController.update);
-postsRouter.delete("/:post", PostController.delete);
-postsRouter.patch("/:post", PostController.like);
+
+postsRouter.use(verifyJwt);
+postsRouter.post("/", PostController.create);
+
+postsRouter
+	.route("/:post")
+	.all(requirePostOwner)
+	.get(PostController.show)
+	.put(PostController.update)
+	.delete(PostController.delete);
+
+postsRouter.patch("/:post/like", PostController.like);
+
 export default postsRouter;
