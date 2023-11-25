e900c659d37a2fc7debf89d29ce804f334f19560
6-ON <aminhatim6@gmail.com>
2023-11-25 18:10:49 +0100

    Add authRouter to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router, json } from "express";
 import cors from "cors";
 import postsRouter from "./posts";
-
+import authRouter from "./auth";
 const router = Router();
+
 router.use(json({ limit: 1024 * 1024 * 10, type: "application/json" }));
 router.use(cors({ origin: "http://localhost:3000" }));
+
 router.use("/posts", postsRouter);
+
+router.use("/auth", authRouter);
 
 export default router;
