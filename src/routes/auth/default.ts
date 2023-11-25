74260657d14ae6f4553b178ac8a5eda3e45a9142
6-ON <aminhatim6@gmail.com>
2023-11-25 18:08:40 +0100

    Add default authentication routes

---
+++ src/routes/auth/default.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { DefaultAuthController } from "@/controllers/auth";
+import { verifyJwt } from "@/middlewares";
+
+const router = Router();
+
+router.post("/login", DefaultAuthController.login);
+router.post("/register", DefaultAuthController.register);
+router.get("/refresh", verifyJwt, DefaultAuthController.refresh);
+export default router;
