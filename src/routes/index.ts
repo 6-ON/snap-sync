5520b95027e33588b6dce9fb011a1dc61c42a271
6-ON <aminhatim6@gmail.com>
2023-12-01 23:10:11 +0100

    Add JWT decoding middleware to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,12 @@ import { Router, json } from "express";
 import cors from "cors";
 import postsRouter from "./posts";
 import authRouter from "./auth";
+import { decodeJwt } from "@/middlewares";
 const router = Router();
 
 router.use(json({ limit: 1024 * 1024 * 10, type: "application/json" }));
 router.use(cors({ origin: "http://localhost:3000" }));
-
+router.use(decodeJwt);
 router.use("/posts", postsRouter);
 
 router.use("/auth", authRouter);
