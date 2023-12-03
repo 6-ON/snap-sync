5b253abb3a19936c3d340926d91f9dd74a8c1f4e
6-ON <aminhatim6@gmail.com>
2023-12-03 02:28:07 +0100

    Add error handling for JsonWebTokenError in verifyJwt middleware

--- src/middlewares/verifyJwt.ts
+++ src/middlewares/verifyJwt.ts
@@ -2,6 +2,7 @@ import { Response, NextFunction, Request } from "express";
 import { Unauthorized, BadRequest } from "http-errors";
 import { verify } from "../utils";
 import { UserService } from "@/services";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 export const verifyJwt = async (
 	req: Request,
@@ -12,8 +13,13 @@ export const verifyJwt = async (
 	if (!req.headers.authorization.startsWith("Bearer ")) throw new BadRequest();
 	const token = req.headers.authorization.split(" ")[1];
 	if (!token) throw new BadRequest();
-	const decoded = await verify(token, process.env.JWT_SECRET!);
-	if (!decoded) throw new Unauthorized();
-	req.user = await UserService.findById(decoded.sub!);
-	next();
+	try {
+		const decoded = await verify(token, process.env.JWT_SECRET!);
+		if (!decoded) throw new Unauthorized();
+		req.user = await UserService.findById(decoded.sub!);
+		next();
+	} catch (err) {
+		if (err instanceof JsonWebTokenError) throw new Unauthorized(err.message);
+		throw err;
+	}
 };
